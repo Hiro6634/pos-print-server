4f9e0626a5ef65afb0bf695e16a2dc4ac17e7f40
Hiro <hiro.suyama@gmail.com>
2022-03-13 11:03:11 -0300

    Implementando el servicio

--- PrintSrv.py
+++ PrintSrv.py
@@ -1,4 +1,21 @@
+import os
+from subprocess import call
+import sys
+import pathlib
 import log4p
+from Singleton import Singleton
+
+
+class WorkerThread(metaclass=Singleton):
+    def __init__(self, callback):
+        self.callback = callback
+        self.running = True
+        logger = log4p.GetLogger(__name__)
+        self.log = logger.logger
+
+    def worker(self):
+        while self.running:
+            tiemPolling = config.loopPollingTime()
 
 class PrintSrv:
     def __init__(self):
@@ -6,11 +23,19 @@ class PrintSrv:
         logger = log4p.GetLogger(__name__,config='./log4p.json')
         self.log =logger.loggger
 
+    def loggerInit(self):
+        if( os.name == 'posix '):
+            logpath='~/AJB/logs/PrintSrv'
+        else:
+            logpath='C:/AJB/logs/PrintSrv'
+        pathlib.Path(logpath, parents=True, exist_ok=True)
+
+srv = PrintSrv()
 
 #def main():
 
 #    pass
 
-#if __name__ == '__main__':
-#    main()
+if __name__ == '__main__':
+    print(sys.version)
     
